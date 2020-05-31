9acaf110958a67d572014658f011d5c024b742f8
root <harryhare@163.com>
2020-05-31 14:13:13 +0000

    clean code

--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -32,24 +32,17 @@ class StageToRedshiftOperator(BaseOperator):
         self.s3_key = s3_key
         self.aws_credentials_id = aws_credentials_id
         self.json = json
-        print("***********")
-        print("kwargs",kwargs)
-        print("s3_key:",s3_key)
 
     def execute(self, context):
         aws_hook = AwsHook(self.aws_credentials_id)
         credentials = aws_hook.get_credentials()
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
-
-        redshift.run("DELETE FROM {}".format(self.table))
         
         self.log.info("Clearing data from destination Redshift table")
         redshift.run("DELETE FROM {}".format(self.table))
 
         self.log.info("Copying data from S3 to Redshift")
         rendered_key = self.s3_key.format(**context)
-        print("rendered_key:",rendered_key)
-        print("context:",context)
         s3_path = "s3://{}/{}".format(self.s3_bucket, rendered_key)
         
         formatted_sql = StageToRedshiftOperator.copy_sql.format(
